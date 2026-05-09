2f3f2613102e7378e47ffa44de0a1a0943dd0109
Charlie Barber <cbarb2747@gmail.com>
2026-05-09 16:47:29 -0700

    Successfullly calling python functiongit status

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
-#include <pybind11/pybind11.h>
+// #include <pybind11/pybind11.h>
+#include <pybind11/embed.h>
 
 namespace py = pybind11;
+using namespace py::literals;
 using namespace  std;
 
 int main() {
-    cout << "Hello" << endl;
+
+    py::scoped_interpreter guard{};
+
+    py::print("Hello, World!"); // use the Python API
+    py::module_ calc = py::module_::import("calculate");
+    py::object result = calc.attr("add")(1, 2);
+    int n = result.cast<int>();
+
+    cout << "Result is " << n << endl;
     return 0;
 }
